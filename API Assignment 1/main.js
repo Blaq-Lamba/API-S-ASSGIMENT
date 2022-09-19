d8cd70eff928f95cf2d8e0cc98e386f5d7e961bb
Jayesimi precious <jayesimiprecious@gmail.com>
2022-09-19 10:29:47 +0100

    api

---
+++ API Assignment 1/main.js
@@ -0,0 +1,18 @@
+const quoteText = document.getElementById('quote-text');
+const quoteTags = document.getElementById('quote-tags');
+const quoteAuthor = document.getElementById('quote-author');
+const genQuoteBtn = document.getElementById('gen-quote-btn');
+
+function randomQuote(){
+    fetch ('https://api.quotable.io/random')
+    .then(respons => respons.json())
+    .then(data => {
+        quoteText.textContent = data.content;
+        quoteTags.textContent = data.tags;
+        quoteAuthor.textContent = `--${data.author}`;
+   });
+}
+randomQuote();
+genQuoteBtn.addEventListener('click', () =>{
+    randomQuote();
+})
